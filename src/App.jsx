3fe0d4df4f688255df376d7fc169f89d20356c54
Mali Gaurav <maligaurav947@gmail.com>
2023-11-26 10:22:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import About from "./pages/About/About";
+import Hero from "./pages/Hero/Hero";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <About />
+    </>
+  );
+}
+
+export default App;
